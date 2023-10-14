3471f3dec8d90be235ac5c7a94457c7c26da749a
Vitalii Dolmatov <Dolmatov-vs@yandex.ru>
2023-10-14 12:13:41 +0300

    create project

---
+++ src/main/java/ru/dolmatov/spring/service/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package ru.dolmatov.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.dolmatov.spring.dao.EmployeeDAO;
+import ru.dolmatov.spring.entity.Employee;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
+}
